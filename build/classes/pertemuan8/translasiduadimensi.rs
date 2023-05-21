dd3abee11f09e64c244cc9d3d435f9231311a0ca
alif <alifdaudf123@gmail.com>
2023-05-21 23:00:03 +0700

    first commit

---
+++ build/classes/pertemuan8/translasiduadimensi.rs
@@ -0,0 +1,6 @@
+Pertemuan8.translasiduadimensi$1
+Pertemuan8.translasiduadimensi$2
+pertemuan8.Pertemuan8
+Pertemuan8.translasiduadimensi
+Pertemuan8.translasiduadimensi$3
+Pertemuan8.translasiduadimensi$4
